a0c738b3d05020034379401c3dcf8591d40dbf4a
unknown <mkg909761@gmail.com>
2024-12-15 19:28:37 +0530

    added template section

--- jsecom_web/src/main.js
+++ jsecom_web/src/main.js
@@ -1,1 +1,6 @@
-import './style.css'
+// import "./style.css";
+import{ product } from "../api/product.js";
+import { showProductContainer } from "./homeproductcards.js";
+
+// Define a function named `showProductContainer` that takes an array of products as input.
+showProductContainer(product);
